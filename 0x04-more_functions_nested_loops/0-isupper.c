bc48ed2203c2bb5c21951207846524871acf6f12
musomw <musomw17@gmail.com>
2022-09-16 16:45:43 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,9 +8,9 @@
 
 int _isupper(int c)
 {
-    if ((c >= 'A') && (c <= 'Z'))
-    {
-        return (1);
-    }
-    return(0);
+	if ((c >= 'A') && (c <= 'Z'))
+	{
+		return (1);
+	}
+	return (0);
 }
