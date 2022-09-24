1707033bb72299d1170f3db9fcc9ac9502aceb31
musomw <musomw17@gmail.com>
2022-09-24 02:38:44 -0400

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*Main - main printing method
+*
+*Return: returns 0
+*/
+
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
