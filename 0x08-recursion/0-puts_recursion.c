c82470adc8c5e38509c414aaf33ac919dc6fff74
musomw <musomw17@gmail.com>
2023-01-04 10:05:19 -0500

    0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,13 +11,13 @@
  */
 void _puts_recursion(char *s)
 {
-        if (*s)
-        {
-                _putchar(*s);
-                _puts_recursion(s + 1);
-        }
+	if (*s)
+	{
+	_putchar(*s);
+	_puts_recursion(s + 1);
+	}
 
-        else
-                _putchar('\n');
+	else
+	_putchar('\n');
 }
 
