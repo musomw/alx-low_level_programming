6ea1a4b029a615a15c4d805e4faf0c203f67383a
musomw <77114951+musomw@users.noreply.github.com>
2023-03-16 21:37:39 -0400

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main(void) {
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
     printf("Thou art a work of art.\n");
-    return 0;
+    return (0);
 }
