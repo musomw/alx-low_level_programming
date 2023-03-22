adb94f90dec325bd4fb2b91ffa96c8ebf4a1b4b1
musomw <77114951+musomw@users.noreply.github.com>
2023-03-22 02:43:55 +0000

    4 5 6 left shift

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,12 +2,12 @@
 
 
 /**
-	* print_sign - prints the sign of a number
-	* @n: the int to check
-	* Return: 1 and prints + if n is greater than zero
-	* 0 and prints 0 if n is zero
-	* -1 and prints - if n is less than zero
-	*/
+* print_sign - prints the sign of a number
+* @n: the int to check
+* Return: 1 and prints + if n is greater than zero
+* 0 and prints 0 if n is zero
+* -1 and prints - if n is less than zero
+*/
 int print_sign(int n)
 {
 	if (n > 0)
