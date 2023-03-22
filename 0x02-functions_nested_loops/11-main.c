e9bafc10cd1ca79ca81c0592429011c7e136308e
musomw <77114951+musomw@users.noreply.github.com>
2023-03-22 02:48:31 +0000

    8 9 10 11 100 101 102 103 left shift betty

--- 0x02-functions_nested_loops/11-main.c
+++ 0x02-functions_nested_loops/11-main.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 
-	/**
-	 * main - check the code
-	 *
-	 * Return: Always 0.
-	 */
-	int main(void)
-	{
-	    print_to_98(0);
-	    print_to_98(98);
-	    print_to_98(111);
-	    print_to_98(81);
-	    print_to_98(-10);
-	    return (0);
-	}
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+	print_to_98(0);
+	print_to_98(98);
+	print_to_98(111);
+	print_to_98(81);
+	print_to_98(-10);
+	return (0);
+}
