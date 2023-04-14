5ff0f4175b975186a0fdc78ff029039963a6d2db
musomw <77114951+musomw@users.noreply.github.com>
2023-04-13 22:56:28 -0400

    Update 1-main.c

--- 0x0C-more_malloc_free/1-main.c
+++ 0x0C-more_malloc_free/1-main.c
@@ -3,17 +3,17 @@
 #include <stdlib.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+* main - check the code
+*
+* Return: Always 0.
+*/
 int main(void)
 {
-    char *concat;
+char *concat;
 
-    concat = string_nconcat("Best ", "School !!!", 6);
-    printf("%s\n", concat);
-    free(concat);
-    return (0);
+concat = string_nconcat("Best ", "School !!!", 6);
+printf("%s\n", concat);
+free(concat);
+return (0);
 }
 
