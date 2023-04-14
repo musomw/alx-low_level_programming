82e7c8670972277d45220db80f5633c3e16664dd
musomw <musomw17@gmail.com>
2023-04-13 20:40:36 -0700

    0 betty style fix

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,37 +5,30 @@
 
 
 /**
-
 * *malloc_checked - allocates memory using malloc
-
 * @b: number of bytes to allocate
-
 *
-
 * Return: a pointer to the allocated memory
-
 */
 
 void *malloc_checked(unsigned int b)
-
 {
 
-void *ptr;
+	void *ptr;
 
 
 
-ptr = malloc(b);
+	ptr = malloc(b);
 
 
 
-if (ptr == NULL)
+	if (ptr == NULL)
 
-	exit(98);
+		exit(98);
 
 
 
-return (ptr);
+	return (ptr);
 
 }
-
 
