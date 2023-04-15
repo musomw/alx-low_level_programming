4bbc0f3c5c79a46fea38ce0818758efc5a9d37e3
musomw <musomw17@gmail.com>
2023-04-14 19:46:10 -0700

    main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,17 +1,17 @@
+#ifndef MAIN_H
+	#define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
 
-int _putchar(char c);
-void *malloc_checke(unsigned int b);
-char *sring_nconcat(char *1s, char *s2, unsigned int n);
-void *_calloc(unsinged int nmemb, unsingned int size);
-int *array_range(int min, intmax);
-void *_realloc(void *ptr, unsinged int old_size unsigned int new_size);
-void errors(void);
-int _strlent(char *s);
-int isdigit(char *s);
+	#include <stdio.h>
+	#include <stdlib.h>
+	#include <limits.h>
 
-#endif
 
+	int _putchar(char c);
+	void *malloc_checked(unsigned int b);
+	char *string_nconcat(char *s1, char *s2, unsigned int n);
+	void *_calloc(unsigned int nmemb, unsigned int size);
+	int *array_range(int min, int max);
+	void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+	#endif
