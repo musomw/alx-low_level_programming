39456baac8d3df2a6a0a637333f31d7949082c50
musomw <77114951+musomw@users.noreply.github.com>
2023-04-17 17:29:52 -0400

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,13 +2,13 @@
 #define DOG_H
 
 /**
- * struct dog - a dog's basic info
- * @name: First member
- * @age: Second member
- * @owner: Third member
- *
- * Description: Longer description
- */
+* struct dog - a dog's basic info
+* @name: First member
+* @age: Second member
+* @owner: Third member
+*
+* Description: Longer description
+*/
 struct dog
 {
 	char *name;
