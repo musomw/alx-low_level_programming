c5bfbf83dcaa29b89f62f4f98728ab280ac481aa
musomw <musomw17@gmail.com>
2023-04-17 14:26:31 -0700

    0E

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,13 @@
+C - Structures, typedef
+
+TASKS
+
+0.Define a new type struct dog with the following elements :
+
+name, type = char * 
+
+age, type = float
+
+owner, type = char *
+
+
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
